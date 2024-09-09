68f0e96a48fe4da757cb0b4d9fa7e024e4b1e559
BrianMohseni <157198911+BrianMohseni@users.noreply.github.com>
2024-09-08 21:32:25 -0400

    Create load_celeba_dataset.py

---
+++ load_celeba_dataset.py
@@ -0,0 +1,14 @@
+import gdown
+import os
+from zipfile import ZipFile
+
+os.makedirs("celeba", exist_ok=True)
+
+url = "https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684"
+output = "celeba_gan/data.zip"
+gdown.download(url, output, quiet=True)
+
+with ZipFile("celeba_gan/data.zip", "r") as zipobj:
+    zipobj.extractall("celeba_gan")
+
+os.remove("celeba/data.zip")
