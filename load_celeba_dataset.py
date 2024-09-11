c986f73aeeca7263eb0b135b97c93b7b15cbde2e
BrianMohseni <157198911+BrianMohseni@users.noreply.github.com>
2024-09-11 16:29:20 -0400

    Update load_celeba_dataset.py

--- load_celeba_dataset.py
+++ load_celeba_dataset.py
@@ -5,7 +5,7 @@ from zipfile import ZipFile
 os.makedirs("celeba", exist_ok=True)
 
 url = "https://drive.google.com/uc?id=1O7m1010EJjLE5QxLZiM9Fpjs7Oj6e684"
-output = "celeba_gan/data.zip"
+output = "celeba/data.zip"
 gdown.download(url, output, quiet=True)
 
 with ZipFile("celeba/data.zip", "r") as zipobj:
